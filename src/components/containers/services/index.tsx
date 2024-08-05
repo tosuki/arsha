88fb2e0a743c2488e7202e12f42df9872d2e5ff8
okaabe <okaabe2006@gmail.com>
2024-08-05 12:04:18 -0300

    feat: add service container, paragraph to the header

--- src/components/containers/services/index.tsx
+++ src/components/containers/services/index.tsx
@@ -1,11 +1,38 @@
+import { ReactNode } from "react"
 import ContainerPrototype from "../prototype"
 
 import "./styles.css"
 
+export function ServiceCard(properties: {
+    title: string
+    icon: ReactNode
+    description: string
+}) {
+    return (
+        <div className="service-card">
+            { properties.icon }
+            <h1>{ properties.title }</h1>
+            <p>{ properties.description }</p>
+        </div>
+    )
+}
+
 export default function ServicesContainer() {
     return (
         <ContainerPrototype>
-            <h1>Hello World</h1>
+            <div className="services-container" id="services">
+                <div className="header">
+                    <h1>Services</h1>
+                    <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+                </div>
+                <div className="services">
+                    <ServiceCard 
+                        icon={(<span>test</span>)}
+                        title="hello"
+                        description="testando alguma coisa"
+                    />
+                </div>
+            </div>
         </ContainerPrototype>
     )
 }
